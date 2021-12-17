6a6a4cf4ebedd21121a146fa0dcae8c3f455a509
Mohamed Hassanin Omran <mohamed_hassanin_omran@yahoo.com>
2021-12-17 22:27:04 +0200

    Add: hog features to get around 80% acuracy

--- featureExtraction.py
+++ featureExtraction.py
@@ -1,5 +1,7 @@
 import numpy as np
 from numpy.core.records import array 
+import cv2
+
 windowWidth = 8 
 numberOfCells = 20
 def getBaseline(img):
@@ -18,7 +20,7 @@ def getCenterOfGravity(window):
     return centerOfGravity
 
 
-def slidingWindowFeatures(img):
+def slidingWindowFeatures(img,grayimg):
     features = []
     # calc base line
     LB , UB = getBaseline(img)
@@ -34,6 +36,7 @@ def slidingWindowFeatures(img):
     # loop through windows
     while x1 >0:
         window = img[max(x2,0):x1,:]
+        graywindow = grayimg[max(x2,0):x1,:]
         x1 = x2
         x2 -= w 
         f1 = 0
@@ -85,6 +88,17 @@ def slidingWindowFeatures(img):
         
         current_feature_vector= [LB,UB,f1,f2,f3,f4,f5,f6,f7,f8]
         current_feature_vector+=list(f21to28)
+        current_feature_vector+=(getHOG(graywindow))
         features.append(current_feature_vector)
     return np.array(features)
     
+def getHOG(img):
+    hist = [0]*8
+    gx = cv2.Sobel(img, cv2.CV_32F, 1, 0, ksize=1)
+    gy = cv2.Sobel(img, cv2.CV_32F, 0, 1, ksize=1)
+    mag, angle = cv2.cartToPolar(gx, gy, angleInDegrees=True)
+    angle /= 45
+    for i in range(mag.shape[0]):
+        for j in range(mag.shape[1]):
+            hist[np.uint16(angle[i,j])] += 1
+    return hist
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import cv2
 from preprocessing import binraization
-from featureExtraction import getBaseline,slidingWindowFeatures
+from featureExtraction import getBaseline,slidingWindowFeatures,getHOG
 import os
 from model import Model
 from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 import numpy as np
 
 # seperate test and train data
@@ -22,40 +23,48 @@ for fnt in range(Model.FONTS_NO):
     for file_name in train[fnt]:
         file_path = f"ACdata_base/{fnt+1}/{file_name}"
         img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
-
+        grayimg = np.copy(img)
+        # HOGs = getHOG(img)
+        # print(HOGs.shape)
         if img[0,0]<150:
             img =255-img
 
         img = binraization(img,t=30)
 
-        output = slidingWindowFeatures(img)
-
+        output = slidingWindowFeatures(img,grayimg)
+        # output = output.mean(axis=0)
+        # histogram of gradients
+        # print(output.shape)
+        # newFeatures = np.concatenate((output,HOGs))
+        
         X.extend(output)
 
+    
+
+
+    # X =preprocessing.normalize(X)
     model.fit(X, fnt)
 
 # test
-
-# train
 accuracies = []
 for fnt in range(Model.FONTS_NO):
     correctly_classified = 0
     for file_name in test[fnt]:
         file_path = f"ACdata_base/{fnt+1}/{file_name}"
         img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
-
+        grayimg = np.copy(img)
         if img[0,0]<150:
             img =255-img
 
         img = binraization(img,t=30)
 
-        output = slidingWindowFeatures(img)
+        output = slidingWindowFeatures(img,grayimg)
 
         label = model.predict(output)
         if label == fnt: correctly_classified += 1
 
     accuracy = correctly_classified / len(test[fnt])
     accuracies.append(accuracy)
-    print(f"font {fnt} accuracy is {accuracy}")
+    print(f"font {fnt+1} accuracy is {accuracy}")
 
 print(f"total accuracy is {np.mean(accuracies)}")
